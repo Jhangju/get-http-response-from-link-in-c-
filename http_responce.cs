1c571599da95f703ffecd81ea14aae378a2d95b0
Jhangju <83189731+white-collor@users.noreply.github.com>
2021-08-24 00:29:35 +0500

    Update http_responce.cs

--- http_responce.cs
+++ http_responce.cs
@@ -14,6 +14,35 @@ namespace http_responce
             string json = new WebClient().DownloadString("https://google.com");
             Console.WriteLine(json);
             Console.ReadLine();
+            
+            
+            
+            try
+            { 
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://google.com");
+
+                req.Method = "POST";
+                string Data = "id=" + comid + "&name=" + cmd + "&result=" + HttpUtility.UrlEncode(text);
+                byte[] postBytes = Encoding.ASCII.GetBytes(Data);
+                req.ContentType = "application/x-www-form-urlencoded";
+                req.ContentLength = postBytes.Length;
+                Stream requestStream = req.GetRequestStream();
+                requestStream.Write(postBytes, 0, postBytes.Length);
+                requestStream.Close();
+
+                HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+                Stream resStream = response.GetResponseStream();
+
+                var sr = new StreamReader(response.GetResponseStream());
+                string responseText = sr.ReadToEnd();
+                Console.WriteLine(responseText);
+
+             }
+            catch (WebException)
+            {
+
+                Console.WriteLine("Please Check Your Internet Connection");
+            }
         }
     }
 }
